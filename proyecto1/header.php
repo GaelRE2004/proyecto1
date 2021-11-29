3dc939ad7643b6ae66648dac301cf010d8659f69
Rodriguez Estrada Gael <91295508+GaelRE2004@users.noreply.github.com>
2021-11-29 10:36:40 -0700

    Add files via upload

---
+++ proyecto1/header.php
@@ -0,0 +1,26 @@
+<?php include("con_db.php"); ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="index.php"><img src="img/lgo.png" height="125px" weight="40px" alt="Gael Rodriguez"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="usuario.php">Registrar Usuario</a></li>
+					<li><a href="mascota.php">Registrar Mascota</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
---
+++ proyecto1/sidebar.php
@@ -0,0 +1,13 @@
+<aside>
+				<div class="widget">
+					<h3>Articulos Relacionados</h3>
+					<ul>
+						<li><a href="https://tec.mx/es/admisiones-y-becas">Admisión y Becas</a></li>
+						<li><a href="https://tec.mx/es/regreso-consciente">Regreso Consciente</a></li>
+						<li><a href="https://tec.mx/es/hyflex-tec">Modelo HyFlex + Tec</a></li>
+						<li><a href="https://tec.mx/es/sentido-humano/sostenibilidad">Tec Sostenible</a></li>
+						<li><a href="https://tec.mx/es/sentido-humano/sostenibilidad">Programas Académicos</a></li>
+						<center>	<img src="img/mas.jpg" height="120px" width="240px"> </center>
+					</ul>
+				</div>
+			</aside>
